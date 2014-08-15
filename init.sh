38ee591592d1e449516edffd43e4b0aaef4c61fb
Mack Stump <mack.stump@gmail.com>
2014-08-15 16:03:10 -0400

    fix potential bugs found by static analyzer

--- init.sh
+++ init.sh
@@ -14,7 +14,7 @@ backup_and_link () {
     echo "Moving $HOME/.$1 to $HOME/.$1.bak"
     mv "$HOME/.$1" "$HOME/.$1.bak"
   fi
-  ln -s $(pwd)/$1 $HOME/.$1
+  ln -s "$(pwd)/$1" "$HOME/.$1"
 }
 
 read -p "Install packages to system (y/N)? "
@@ -24,18 +24,18 @@ git submodule update --init
 
 # ZSH
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+  git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"
 fi
 
 for file in zsh/*; do
   [ -e "$file" ] || continue
 
-  if [ -L "$HOME/.oh-my-zsh/custom/$(basename $file)" ]; then
-    rm "$HOME/.oh-my-zsh/custom/$(basename $file)"
+  if [ -L "$HOME/.oh-my-zsh/custom/$(basename "$file")" ]; then
+    rm "$HOME/.oh-my-zsh/custom/$(basename "$file")"
   fi
 
-  if [ -f "$file" ] && [ ! -f "$HOME/.oh-my-zsh/custom/$(basename $file)" ]; then
-    ln -s $(pwd)/zsh/$(basename $file) "$HOME/.oh-my-zsh/custom/$(basename $file)"
+  if [ -f "$file" ] && [ ! -f "$HOME/.oh-my-zsh/custom/$(basename "$file")" ]; then
+    ln -s "$(pwd)/zsh/$(basename "$file")" "$HOME/.oh-my-zsh/custom/$(basename "$file")"
   fi
 done
 
@@ -56,9 +56,9 @@ backup_and_link 'vim'
 backup_and_link 'vimrc'
 
 vimrc_plugins=$(mktemp)
-sed -n '/set nocompatible/,/call vundle#end/p' vimrc > $vimrc_plugins
-vim -u $vimrc_plugins +PluginInstall +qall
-rm $vimrc_plugins
+sed -n '/set nocompatible/,/call vundle#end/p' vimrc > "$vimrc_plugins"
+vim -u "$vimrc_plugins +PluginInstall +qall"
+rm "$vimrc_plugins"
 
 # fonts
 mkdir -p "$HOME/.fonts"
--- scripts/copy_term.sh
+++ scripts/copy_term.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 if [ -z "$1" ]; then
   echo "Usage: $0 <remote_host>"
@@ -6,5 +6,5 @@ fi
 
 REMOTE_HOST=$1
 
-ssh $REMOTE_HOST "mkdir -p ~/.terminfo/${TERM:0:1}"
+ssh "$REMOTE_HOST" "mkdir -p ~/.terminfo/${TERM:0:1}"
 scp "/usr/share/terminfo/${TERM:0:1}/$TERM" "$REMOTE_HOST:~/.terminfo/${TERM:0:1}"
